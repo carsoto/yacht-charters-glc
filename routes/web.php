13e67429c8f6c146840c7a96cd756be6126a96bb
Carmen Soto <dev.carmensoto@gmail.com>
2022-02-03 21:21:00 -0500

    Jetstream --teams installed

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/test', function () {
     return 'Hello world';
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
