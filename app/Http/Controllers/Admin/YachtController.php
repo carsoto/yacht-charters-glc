dc906806abcba7ce4996b81fb43d0804d5ec7b2f
Carmen Soto <dev.carmensoto@gmail.com>
2022-02-17 06:06:10 -0500

    Estructures for yachts

---
+++ app/Http/Controllers/Admin/YachtController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Admin\Yacht;
+use Illuminate\Http\Request;
+
+class YachtController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Admin\Yacht  $yacht
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Yacht $yacht)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Admin\Yacht  $yacht
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Yacht $yacht)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Admin\Yacht  $yacht
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Yacht $yacht)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Admin\Yacht  $yacht
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Yacht $yacht)
+    {
+        //
+    }
+}
--- resources/views/admin/yacht/index.blade.php
+++ resources/views/admin/yacht/index.blade.php
@@ -1,6 +1,6 @@
 @extends('adminlte::page')
 
-@section('title', 'Reports')
+@section('title', 'Yacht')
 
 @section('content_header')
     <!--<h1>Listado de usuarios</h1>-->
@@ -10,7 +10,7 @@
 
 <div class="card">
     <div class="card-header">
-        <h1>ADMIN <div class="float-right text-xl"><a class="dt_actions"><i class="fas fa-plus"></i></a></div></h1>
+        <h1>YACHT <div class="float-right text-xl"><a class="dt_actions"><i class="fas fa-plus"></i></a></div></h1>
     </div>
     <div class="card-body">
         <div class="row">
